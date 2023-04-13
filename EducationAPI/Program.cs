c7e5067b50c8f3333d4597a1a0f265b9d8b697a7
micha <harasim1999@gmail.pl>
2023-04-13 21:55:44 +0200

    EA-5

--- EducationAPI/Program.cs
+++ EducationAPI/Program.cs
@@ -4,14 +4,44 @@ using System.Reflection;
 using EducationAPI.Services.EducationalSubject;
 using EducationAPI.Middleware;
 using EducationAPI.Services;
-using EducationAPI.Services.User;
+using EducationAPI.Services.UserServices;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using EducationAPI.Models.User.Validators;
+using FluentValidation;
+using EducationAPI.Models.User;
+using FluentValidation.AspNetCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+var authenticationSettings = new AuthenticationSettings();
+
+builder.Configuration.GetSection("Authentication").Bind(authenticationSettings);
+
+builder.Services.AddSingleton(authenticationSettings);
+builder.Services.AddAuthentication(option =>
+{
+    option.DefaultAuthenticateScheme = "Bearer";
+    option.DefaultScheme = "Bearer";
+    option.DefaultChallengeScheme = "Bearer";
+}).AddJwtBearer(cfg =>
+{
+    cfg.RequireHttpsMetadata = false;
+    cfg.SaveToken = true;
+    cfg.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidIssuer = authenticationSettings.JwtIssuer,
+        ValidAudience = authenticationSettings.JwtIssuer,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(authenticationSettings.JwtKey)),
+    };
+});
+
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddFluentValidation();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<EducationDbContext>();
@@ -22,6 +52,8 @@ builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 builder.Services.AddScoped<IEducationalSubjectServices, EducationalSubjectServices>();
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<ErrorHandlingMiddleware>();
+builder.Services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
+builder.Services.AddScoped<IValidator<UserDto>, UserDtoValidator>();
 
 
 var app = builder.Build();
@@ -34,6 +66,7 @@ var seeder = scope.ServiceProvider.GetRequiredService<EducationalSubjectSeeder>(
 
 seeder.Seed();
 
+app.UseAuthentication();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
